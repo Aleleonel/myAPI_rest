b29bd410d340ac12bf46f00eeeea2bc344f4797d
Aleleonel <aleleonel@gmail.com>
2021-09-26 17:15:01 -0300

    validação de campos

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -1,5 +1,8 @@
+from clientes.validators import celular_valido, cpf_valido, nome_valido, rg_valido
+import clientes
 from rest_framework import serializers
 from clientes.models import Cliente
+from clientes.validators import *
 
 
 class ClienteSerializer(serializers.ModelSerializer):
@@ -7,3 +10,19 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields =  '__all__'
+
+    def validate(self, data):
+
+        if not cpf_valido(data['cpf']):
+            raise serializers.ValidationError({'cpf':"CPF inválido, deve conter 11 digitos"})        
+        
+        if not nome_valido(data['nome']):
+            raise serializers.ValidationError({'nome':"Nome de ser uma letras"})
+        
+        if not rg_valido(data['rg']):
+            raise serializers.ValidationError("Quantidade de dígitos inválido par o campo RG")
+        
+        if not celular_valido(data['celular']):
+             raise serializers.ValidationError({'celular':"Celular inválido, favor verificar o número e padrão correto xx xxxx-xxxx"})
+        
+        return data
---
+++ clientes/validators.py
@@ -0,0 +1,20 @@
+import re
+from validate_docbr import CPF
+
+
+def cpf_valido(numero_cpf):
+    cpf = CPF() 
+    return  cpf.validate(numero_cpf)
+       
+def nome_valido(nome):
+    return nome.isalpha()
+
+def rg_valido(rg):
+    return len(rg) == 9
+
+def celular_valido(celular):
+    """Verifica se numero do celular é válido com regex"""
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, celular)
+    return resposta
+        
